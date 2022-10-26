b77d73f242eb71ec41462d5a7fa8c19457f7dfaf
Charles Ukasoanya <c_ukasoanya@yahoo.com>
2022-10-26 14:09:59 +0100

    created HeroBanner component content

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
 
+import { client } from '../lib/client';
+
 import { Product, FooterBanner, HeroBanner } from '../components';
 
-const Home = () => {
+const Home = ({ products, bannerData }) => {
   return (
     <>
-      <HeroBanner />
-
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
       <div className="products-heading">
         <h2>Best Selling Products</h2>
         <p>Speakers of many variations</p>
       </div>
       <div className="products-container">
-        {['Product 1', 'Product 2', 'Product 3'].map((product) => product)}
+        {products?.map((product) => product.name)}
       </div>
 
       <FooterBanner />
@@ -21,3 +22,15 @@ const Home = () => {
 };
 
 export default Home;
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const bannerQuery = '*[_type == "banner"]';
+
+  const products = await client.fetch(query);
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData },
+  };
+};
